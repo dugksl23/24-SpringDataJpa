de929306541baaab5eead6c47f7a136bcb88ebf0
dugksl23 <dugksl23@gmail.com>
2024-05-20 12:41:35 +0900

    jpa 기반 memberJpaRepository 생성

--- src/main/java/study/springdatajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/springdatajpa/repository/MemberJpaRepository.java
@@ -2,12 +2,14 @@ package study.springdatajpa.repository;
 
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jdk.jfr.Registered;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import study.springdatajpa.entity.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -22,5 +24,19 @@ public class MemberJpaRepository {
 
     public void save(Member member) {
         em.persist(member);
+    }
+
+    public Long count(){
+        String jpql = "select count(m) from Member m";
+        return em.createQuery(jpql, Long.class).getSingleResult();
+    }
+
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll(){
+        String jpql = "select m from Member m";
+        return em.createQuery(jpql, Member.class).getResultList();
     }
 }
